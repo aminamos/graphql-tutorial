0a5745bbcbf64afe898123c556337c40d9c230d2
aminamos <26092352+aminamos@users.noreply.github.com>
2020-02-26 02:19:03 -0800

    add an alternate way to define the allLinks query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,7 @@ module Types
   class QueryType < BaseObject
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
+    # field :all_links, [LinkType], null: false
     field :all_links, [LinkType], resolver: Resolvers::LinksSearch, null: false
 
     # this method is invoked, when `all_link` fields is being resolved
